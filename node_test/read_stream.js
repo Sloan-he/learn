681cfe7d5f4ac76822f3aa501a119a1da506518f
hesy <heshy@caibab.com>
2016-07-20 22:57:57 +0800

    7-19learn

--- node_test/read_stream.js
+++ node_test/read_stream.js
@@ -2,18 +2,18 @@
 
 var fs = require('fs');
 
-// 打开一个流
 var rs = fs.createReadStream('sample.txt','utf-8');
 
 rs.on('data',function(chunk){
-	console.log('DATA');
+	console.log('DATA:');
 	console.log(chunk);
 });
+
 
 rs.on('end',function(){
 	console.log('END');
 });
 
 rs.on('error',function(err){
-	console.log('Error:' + err);
+	console.log('ERROR:' + err);
 });
