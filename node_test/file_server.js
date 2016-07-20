75b201017e47b94536a1afa2f40e1b7eee18a9c3
hesy <heshy@caibab.com>
2016-07-21 00:02:21 +0800

    7-21learn

--- node_test/file_server.js
+++ node_test/file_server.js
@@ -14,14 +14,14 @@ console.log('Static root dir:' + root);
 
 //创建服务器
 
-var server = http.createServer(function(request,reponse){
+var server = http.createServer(function(request,response){
 
 	//获得url的path.类似'/css/bootstrap.css'
 	var pathname = url.parse(request.url).pathname;
 
 	//获取对应的本地文件路径,类似'/srv/www/css/bootstrap.css'
 	var filepath = path.join(root,pathname);
-
+	console.log(filepath);
 	//获取文件状态
 	fs.stat(filepath,function(err,stats){
 		if(!err && stats.isFile()){
@@ -29,15 +29,21 @@ var server = http.createServer(function(request,reponse){
 			// 没有出错并且文件存在
 			console.log('200' + request.url);
 			// 发送200状态
-			reponse.writeHead(200);
+			response.writeHead(200);
 
 			//将文件流导向response
-			fs.creatReadStream(filepath).pipe(reponse);
+			fs.creatReadStream(filepath).pipe(response);
 		}else{
 
 			// 出错了或者文件不存在
-			console.log('404' + reponest.url);
+			console.log('404' + request.url);
+			//发送404响应
+			response.writeHead(404);
 			response.end('404 Not Fount');
 		}
 	});
 });
+
+server.listen(8080);
+
+console.log('Server is running at http://127.0.0.1:8080');
