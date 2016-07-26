1a733aa0a54772b0f1df236a822bb840c2a12e42
hesy <heshy@caibab.com>
2016-07-26 21:44:23 +0800

    1

--- node_test/crypto.js
+++ node_test/crypto.js
@@ -1,4 +1,9 @@
 const crypto = require('crypto');
 
-const hash = crypto.creatHash('md5');
+const hash = crypto.createHash('md5');
 
+hash.update('Hello, world!');
+hash.update('Hello, nodejs!');
+
+
+console.log(hash.digest('hex'));
