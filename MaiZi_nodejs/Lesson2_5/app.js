b6a49f13eb4afada5ece87b8b19c183f3bde7cac
hesy <heshy@caibab.com>
2016-07-26 23:52:41 +0800

    7-26learn nodejs

--- MaiZi_nodejs/Lesson2_5/app.js
+++ MaiZi_nodejs/Lesson2_5/app.js
@@ -1,12 +1,44 @@
-var http = require('http');
+﻿var http = require('http');
+
+//初始化路由
+var handlePaths = [];
+
+
+//添加路由地址
+function initPaths(){
+
+	handlePaths.push('/');
+	handlePaths.push('/login');
+	handlePaths.push('/register');
+
+}
+
+//判断路由地址
+function rotuteHandle(path){
+
+	for(var i in handlePaths){
+		console.log(handlePaths[i],path);
+		if(handlePaths[i] == path){
+			console.log(handlePaths[i]);
+			return handlePaths[i];
+		}
+	}
+	return '404 Not Fount';
+}
+
 
 
 function onRequest(request,response){
+
 	response.writeHead(200,{'Content-Type':'text/plain'});
 	var requestPath = request.url;
+	var responseContent = rotuteHandle(requestPath);
+	response.write('rotute is:' + responseContent);
 	response.end();
+
 }
 
+initPaths();
 
 var server = http.createServer(onRequest);
 server.listen(8080);
---
+++ MaiZi_nodejs/Lesson2_6/app.js
@@ -0,0 +1,24 @@
+﻿
+
+/*console.log('循环开始');
+
+var i = 10;
+while( i > 0 ){
+	i --;
+	console.log(i);
+}
+
+console.log('循环结束');*/
+
+
+console.log('循环开始');
+
+setTimeout(function(){
+	var i = 10;
+	while( i > 0 ){
+		i --;
+		console.log(i);
+	}
+},0);
+
+console.log('循环结束');
