ab895829c4c84ad446312be3064d5216f972eede
hesy <heshy@caibab.com>
2016-09-20 18:07:26 +0800

    9-18 learn react

--- react/react-docs/thinking-in-react/example.js
+++ react/react-docs/thinking-in-react/example.js
@@ -2,7 +2,7 @@
  * Created by hesy on 2016/9/19.
  */
 
-//创建表格分类组件
+//创建表格分类行组件
 var ProductCategoryRow = React.createClass({
     render:function(){
         return <tr><th colSpan="2">{this.props.category}</th></tr>
@@ -13,6 +13,7 @@ var ProductCategoryRow = React.createClass({
 //创建每行显示数据组件
 var ProductRow = React.createClass({
     render:function(){
+        //判断库存
         var name = this.props.product.stocked ?
             this.props.product.name :
             <span style={{color:'red'}}>{this.props.product.name}</span>;
@@ -27,15 +28,18 @@ var ProductTable = React.createClass({
         var rows = [];
         var lastCategory = null;
         this.props.products.forEach(function(product){
-            //if(product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)){
-            //    return;
-            //}
+            //判断文本框的值和是否勾选了仅显示有库存并且该货品是否有库存
+            if(product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)){
+                return;
+            }
+            //判断分类
             if(product.category !== lastCategory){
                 rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
             }
             rows.push(<ProductRow product={product} key={product.name} />);
+            //修改当前分类
             lastCategory = product.category;
-        });
+        }.bind(this));
         return (
             <table>
                 <thead>
@@ -52,11 +56,18 @@ var ProductTable = React.createClass({
 
 //创建搜索组件
 var SearchBar = React.createClass({
+    //表单变化改变时修改state
+    handleChange:function(){
+        this.props.onUserInput(
+            this.refs.filterTextInput.value,
+            this.refs.inStockOnlyInput.checked
+        );
+    },
     render:function(){
         return (
             <form action="">
-                <input type="text" placeholder="Search..." value={this.props.filterText} />
-                <p><input type="checkbox" checked={this.props.inStockOnly} />{' '}Only show products in stock</p>
+                <input type="text" placeholder="Search..." value={this.props.filterText} ref="filterTextInput" onChange={this.handleChange} />
+                <p><input type="checkbox" checked={this.props.inStockOnly} ref="inStockOnlyInput" onChange={this.handleChange} />{' '}Only show products in stock</p>
             </form>
         );
     }
@@ -70,10 +81,17 @@ var FilterableProductTable = React.createClass({
             inStockOnly:false
         }
     },
+    //表单变化时事件
+    handleUserInput:function(filterText,inStockOnly){
+        this.setState({
+            filterText: filterText,
+            inStockOnly: inStockOnly
+        });
+    },
     render:function(){
         return (
             <div>
-                <SearchBar filterText={this.state.filterText} inStockOnly={this.state.inStockOnly}  />
+                <SearchBar filterText={this.state.filterText} inStockOnly={this.state.inStockOnly} onUserInput={this.handleUserInput}  />
                 <ProductTable products={this.props.products} filterText={this.state.filterText} inStockOnly={this.state.inStockOnly} />
             </div>
         );
