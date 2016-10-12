b32dfb26edca42231b88ab14f895659d39d08677
hesy <heshy@caibab.com>
2016-10-13 00:01:17 +0800

    1

--- redux-tutorial-cn/test_07.js
+++ redux-tutorial-cn/test_07.js
@@ -1,14 +1,40 @@
-/**
- * Created by hesy on 2016/10/12.
- */
-
 import {createStore,combineReducers} from 'redux'
 
 var reducer = combineReducers({
   speaker:function(state = {},action){
     console.log('speaker state:',state,'action:',action)
     switch (action.type){
-
+    	case 'SAY':
+    		return {
+    			...state,
+    			message:action.message
+    		}
+    	default:return state;
     }
   }
 })
+
+var store_0 = createStore(reducer);
+
+var sayActionCreator = function(mesasge){
+	return {
+		type:'SAY',
+		mesasge
+	}
+}
+
+store_0.dispatch(sayActionCreator('Hi'))
+console.log('store_0 state after action SAY:', store_0.getState())
+
+//-------------------------------------------------------------------
+
+var ansncSayActionCreator_0 = function(message){
+	return function(dispatch){
+		setTimeout(function(){
+			return dispatch({
+				type:'SAY',
+				message
+			})
+		},2000)
+	}
+}
---
+++ redux-tutorial-cn/test_08.js
@@ -0,0 +1,31 @@
+import { createStore, combineReducers } from 'redux'
+
+var reducer = combineReducers({
+    speaker: function (state = {}, action) {
+        console.log('speaker was called with state', state, 'and action', action)
+
+        switch (action.type) {
+            case 'SAY':
+                return {
+                    ...state,
+                    message: action.message
+                }
+            default:
+                return state;
+        }
+    }
+})
+var store_0 = createStore(reducer)
+
+var asyncSayActionCreator_1 = function (message) {
+    return function (dispatch) {
+        setTimeout(function () {
+            dispatch({
+                type: 'SAY',
+                message
+            })
+        }, 2000)
+    }
+}
+
+store_0.dispatch(asyncSayActionCreator_1('Hi'))
