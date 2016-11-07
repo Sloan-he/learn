85cc3715f274d541c8331cd58fb4bfb3cbe99741
hesy <heshy@caibab.com>
2016-11-07 23:32:57 +0800

    11-07 learn redux

--- redux_org/redux/examples/todos/src/components/Dialog.js
+++ redux_org/redux/examples/todos/src/components/Dialog.js
@@ -4,24 +4,35 @@ import {toggleTodo} from '../actions/index'
 import {connect} from 'react-redux'
 
 class Dialog extends Component{
-  constructor(props){
-    super(props)
-    this.state = {editId:props.editId}
-  }
+   constructor(props){
+   		super(props)
+     	this.state = {editText:props.editText,editId:props.editId}
+     	this.changeValue = this.changeValue.bind(this)
+    }
+    componentWillReceiveProps(nextProps){
+    	this.setState({
+    		editText:nextProps.editText,
+    		editId:nextProps.editId
+    	});
+    }
+    changeValue = () =>{
+    	this.setState({editText:this.refs.input.value});
+    }
 	render(){
 		const {dispatch} = this.props
 		return (
 			<div>
 				<form onSubmit={(e) =>{
 					e.preventDefault();
-					console.log(this.state.editId);
-					if(true && this.state.editId !== null){
-						dispatch(toggleTodo(this.props.editId,'000000000'))
+					if(this.state.editText.trim() && this.state.editId !== null){
+						dispatch(toggleTodo(this.props.editId,this.state.editText))
 					}
-					//this.setState({editId:null})
+					this.setState({
+						editId:null,
+						editText:''
+					})
 				}}>
-					<input type="text" ref="input" />
-          {this.props.editId}
+					<input type="text" ref="input" value={this.state.editText} onChange={this.changeValue} />
 					<button type="submit">提交</button>
 				</form>
 			</div>
