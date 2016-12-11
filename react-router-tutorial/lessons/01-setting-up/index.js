3aa6dd69f3e05e91bd420a213292b6422ebe5f53
hesy <heshy@caibab.com>
2016-12-12 00:35:31 +0800

    12-11 learn react-router

--- react-router-tutorial/lessons/01-setting-up/index.js
+++ react-router-tutorial/lessons/01-setting-up/index.js
@@ -5,13 +5,21 @@ import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
 import Repo from './modules/Repo'
+import AboutMessage from './modules/About/Message'
+import Message from './modules/Message'
 
 render((
   <Router history={hashHistory}>
-    <Route path="/(:id)" component={App}>
+    <Route path="/" component={App}>
       <Route path="/repos" component={Repos}/>
-      <Route path="/repos/:userName/:repoName" component={Repo}/>
-      <Route path="/about/:id" component={About}/>
+      <Route path="/repos/:userName/:repoName" component={Repo}/>  
+      <Route path="/about" component={About}>
+      	<Route path="/abmessage/:id" component={AboutMessage} />
+      </Route>
     </Route>
+    <Route path="/message" component={Message} />
+	<Route path="/message2" component={Message} />
+
+    
   </Router>
 ), document.getElementById('app'))
